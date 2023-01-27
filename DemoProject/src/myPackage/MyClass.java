12af470643c12d0c129dd6149b8668c0ab4f6fe9
smitavd <smitavd8@gmail.com>
2023-01-27 23:51:07 +0530

    Basic Task

---
+++ DemoProject/src/myPackage/MyClass.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+public class MyClass {
+
+	public static void main(String[]args)
+	{
+		System.out.println("TestBase");
+		System.out.println("POM.xml");
+		System.out.println("Utilities");
+		System.out.println("Resource");
+		System.out.println("Suite");
+	}
+}
